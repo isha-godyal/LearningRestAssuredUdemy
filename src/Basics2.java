64a31776a9485a7632595f33d7e677c398bc31a8
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-06-15 15:53:04 +0530

    json body is passed from another package with method name AddPlace() from class Payload

--- src/Basics2.java
+++ src/Basics2.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
 import static org.hamcrest.Matchers.*;
+
+import files.Payload;
+
 import static io.restassured.RestAssured.*;
 
 public class Basics2 {
@@ -19,14 +22,7 @@ public class Basics2 {
 		 */
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body("{\r\n" + "\r\n" + "    \"location\":{\r\n" + "\r\n" + "        \"lat\" : -38.383494,\r\n"
-						+ "\r\n" + "        \"lng\" : 33.427362\r\n" + "\r\n" + "    },\r\n" + "\r\n"
-						+ "    \"accuracy\":50,\r\n" + "\r\n" + "    \"name\":\"Rahul SHetty Academy\",\r\n" + "\r\n"
-						+ "    \"phone_number\":\"(+91) 983 893 3937\",\r\n" + "\r\n"
-						+ "    \"address\" : \"29, side layout, cohen 09\",\r\n" + "\r\n"
-						+ "    \"types\": [\"shoe park\",\"shop\"],\r\n" + "\r\n"
-						+ "    \"website\" : \"http://google.com\",\r\n" + "\r\n"
-						+ "    \"language\" : \"French-IN\"\r\n" + "\r\n" + "}\r\n" + "")
+				.body(Payload.AddPlace())
 				.when().post("maps/api/place/add/json").then().log().all().assertThat().statusCode(200)
 				.body("scope", equalTo("APP")).header("server", "Apache/2.4.41 (Ubuntu)");
 	}
