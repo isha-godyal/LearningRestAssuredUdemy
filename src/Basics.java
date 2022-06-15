03f2482ff719b50183ca73c0b6232c16ec0a8c57
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-06-15 14:30:55 +0530

    build Rest API automation test to add place and validate status code

---
+++ src/Basics.java
@@ -0,0 +1,45 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class Basics {
+
+	public static void main(String[] args) {
+
+		//validate if ADD Place API is working as expected
+		
+		//given - all input details
+		//when - Submit the API --> resource,http method
+		//Then - validate the response
+		
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "\r\n"
+				+ "    \"location\":{\r\n"
+				+ "\r\n"
+				+ "        \"lat\" : -38.383494,\r\n"
+				+ "\r\n"
+				+ "        \"lng\" : 33.427362\r\n"
+				+ "\r\n"
+				+ "    },\r\n"
+				+ "\r\n"
+				+ "    \"accuracy\":50,\r\n"
+				+ "\r\n"
+				+ "    \"name\":\"Rahul SHetty Academy\",\r\n"
+				+ "\r\n"
+				+ "    \"phone_number\":\"(+91) 983 893 3937\",\r\n"
+				+ "\r\n"
+				+ "    \"address\" : \"29, side layout, cohen 09\",\r\n"
+				+ "\r\n"
+				+ "    \"types\": [\"shoe park\",\"shop\"],\r\n"
+				+ "\r\n"
+				+ "    \"website\" : \"http://google.com\",\r\n"
+				+ "\r\n"
+				+ "    \"language\" : \"French-IN\"\r\n"
+				+ "\r\n"
+				+ "}\r\n"
+				+ "").when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200);
+	}
+
+}
